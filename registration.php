7ded7db5813e8f188bcc2f88b6f4b37407628433
Alexander Tolochko <twist999@gmail.com>
2016-08-22 16:47:03 +0300

    Init new module

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2016 MagestyApps. All rights reserved.
+ *  * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MagestyApps_FixRules',
+    __DIR__
+);
